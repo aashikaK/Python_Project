ec0ab6816029b6d4256b3c5dd4142c8463b873e6
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 19:55:14 +0545

    snake,water,gun

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ yourChoice=input('Enter your choice(s for snake,w for water and g for gun): ')
 yourDict={"s":1,"w":-1,"g":0}
 you=yourDict[yourChoice]
 
-if(computer==-1 and you==1):
+elif(computer==-1 and you==1):
     print("You win!")
 
 elif(computer==-1 and you==0):
     print("You lose!")
 
-if(computer==1 and you==-1):
+elif(computer==1 and you==-1):
     print("You lose!")
 
 elif(computer==1 and you==0):
     print("You win!")
 
-if(computer==0 and you==-1):
+elif(computer==0 and you==-1):
     print("You win!")
 
 elif(computer==0 and you==1):
