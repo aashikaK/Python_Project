a8278145bcf13be5ddbefb88bc54c1eb20f081cf
Aashika <khatiwadaaashika12@gmail.com>
2026-02-26 20:22:40 +0545

    snake,water,gun

--- shortMain.py
+++ shortMain.py
@@ -15,23 +15,31 @@ print(f'You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}')
 
 if(computer==you):
     print("It is a draw.")
-elif(computer==-1 and you==1):
-    print("You win!")
+# elif(computer==-1 and you==1): #computer-you = -2
+#     print("You win!")
 
-elif(computer==-1 and you==0):
+# elif(computer==-1 and you==0):#computer-you = -1
+#     print("You lose!")
+
+# elif(computer==1 and you==-1):#computer-you = 2
+#     print("You lose!")
+
+# elif(computer==1 and you==0):#computer-you = 1
+#     print("You win!")
+
+# elif(computer==0 and you==-1):#computer-you = 1
+#     print("You win!")
+
+# elif(computer==0 and you==1):#computer-you =  -1
+#     print("You lose!")
+
+# you lose when comp-you=-1 and 2 and win when comp-you is 1 and -2 as per the pattern so below code
+# based on that logic
+
+elif((computer-you)==-1 or(computer-you)==2):
     print("You lose!")
 
-elif(computer==1 and you==-1):
-    print("You lose!")
-
-elif(computer==1 and you==0):
+elif((computer-you)==1 or(computer-you)==-2):
     print("You win!")
-
-elif(computer==0 and you==-1):
-    print("You win!")
-
-elif(computer==0 and you==1):
-    print("You lose!")
-
 else: 
     print('Something went wrong!!')
